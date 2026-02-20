32ff0ad3707cb464cad42f12c26155f24b538fe4
letskoo <kiwankoo@naver.com>
2026-02-21 01:47:33 +0900

    FORCE CORS HEADER

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,15 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    credentials: true,
+  // 🔥 강제 CORS 헤더
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
   });
 
   app.useGlobalFilters(new GlobalExceptionFilter());
