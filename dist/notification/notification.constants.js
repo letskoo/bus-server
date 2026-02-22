92d9ea5e551477aa6e0e33d709826819dea48213
letskoo <kiwankoo@naver.com>
2026-02-22 21:08:11 +0900

    stop tracking node_modules

--- dist/notification/notification.constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SHARE_TOKEN_TTL_MINUTES_DEFAULT = exports.NOTIFICATION_POINTS = void 0;
-exports.NOTIFICATION_POINTS = {
-    ALIMTALK: 10,
-    SMS: 50,
-};
-exports.SHARE_TOKEN_TTL_MINUTES_DEFAULT = 30;
-//# sourceMappingURL=notification.constants.js.map
